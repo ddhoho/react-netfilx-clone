a04a6216d42575c953e7ab3da17c0f1c01d76cc4
ddhoho <wnsdla539@gmail.com>
2024-03-16 18:44:41 +0900

    230316 넷플릭스 클론 코딩

---
+++ src/api/requests.js
@@ -0,0 +1,13 @@
+const requests = {
+    fetchNowPlaying: "movie/now_playing",
+    fetchNetflixOriginals: "/discover/tv?with_networks=213",
+    fetchTrending: "/trending/all/week",
+    fetchTopRated: "/movie/top_rated",
+    fetchActionMovie: "/discover/movie?with_genres=28",
+    fetchComedyMovie: "/discover/movie?with_genres=35",
+    fetchHorrorMovie: "/discover/movie?with_genres=27",
+    fetchRomanceMovie: "/discover/movie?with_genres=10749",
+    fetchDocumentaries: "/discover/movie?with_genres=99",
+}
+
+export default requests;
